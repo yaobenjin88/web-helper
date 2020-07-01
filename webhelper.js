ff87818ad04b1463fea4a5303cde5a7dfac5e783
yaobenjin@maxwin.com.tw <yaobenjin@maxwin.com.tw>
2020-07-01 16:23:07 +0800

    ok

---
+++ webhelper.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         web-helper
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.sohu.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     document.title='ÎÒµÄsohu'
+    // Your code here...
+})();
